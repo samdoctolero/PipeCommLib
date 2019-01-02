7d059be13e9e204855c5afeb18b96d15c1905e0e
Sam <scdoctol@ucalgary.ca>
2019-01-01 18:10:44 -0700

    added spaces for pointers

--- PipeCommLib/CPipeComm.cpp
+++ PipeCommLib/CPipeComm.cpp
@@ -2,74 +2,74 @@
 #include "CPipeComm.h"
 #include "PipeComm.h"
 
-TxPipeComm* CreateCTxPipeComm(char* name, void* msg)
+TxPipeComm * CreateCTxPipeComm(char * name, void * msg)
 {
 	std::string strName = name;
-	return new TxPipeComm(strName, (IMessage*)msg);
+	return new TxPipeComm(strName, (IMessage *)msg);
 }
 
-void DestroyCTxPipeComm(TxPipeComm* ptr)
+void DestroyCTxPipeComm(TxPipeComm * ptr)
 {
 	delete ptr;
 }
 
-void Send(TxPipeComm* ptr, void* msg) 
+void Send(TxPipeComm * ptr, void * msg) 
 {
-	ptr->Send((IMessage*)msg);
+	ptr->Send((IMessage *)msg);
 }
 
-void StartTx(TxPipeComm* ptr)
+void StartTx(TxPipeComm * ptr)
 {
 	ptr->Start();
 }
 
-void StopTx(TxPipeComm* ptr)
+void StopTx(TxPipeComm * ptr)
 {
 	ptr->Stop();
 }
 
-void ResetTx(TxPipeComm* ptr)
+void ResetTx(TxPipeComm * ptr)
 {
 	ptr->Reset();
 }
 
-int IsRunningTx(TxPipeComm* ptr)
+int IsRunningTx(TxPipeComm * ptr)
 {
 	return ptr->IsRunning();
 }
 
-RxPipeComm* CreateCRxPipeComm(char* name, void* msg)
+RxPipeComm * CreateCRxPipeComm(char * name, void * msg)
 {
 	std::string strName = name;
-	return new RxPipeComm(strName, (IMessage*)msg);
+	return new RxPipeComm(strName, (IMessage *)msg);
 }
 
-void DestroyCRxPipeComm(RxPipeComm* ptr)
+void DestroyCRxPipeComm(RxPipeComm * ptr)
 {
 	delete ptr;
 }
 
-void* Get(RxPipeComm* ptr)
+void * Get(RxPipeComm * ptr)
 {
-	return (void*)ptr->Get();
+	return (void *)ptr->Get();
 }
 
-void StartRx(RxPipeComm* ptr)
+void StartRx(RxPipeComm * ptr)
 {
 	ptr->Start();
 }
 
-void StopRx(RxPipeComm* ptr)
+void StopRx(RxPipeComm * ptr)
 {
 	ptr->Stop();
 }
 
-void ResetRx(RxPipeComm* ptr)
+void ResetRx(RxPipeComm * ptr)
 {
 	ptr->Reset();
 }
 
-int IsRunningRx(RxPipeComm* ptr)
+int IsRunningRx(RxPipeComm * ptr)
 {
 	return ptr->IsRunning();
 }
--- PipeCommLib/CPipeComm.h
+++ PipeCommLib/CPipeComm.h
@@ -12,15 +12,15 @@ extern "C"
 	typedef struct TxPipeComm TxPipeComm;
 
 	// TxPipeComm constructor/destructor
-	TxPipeComm* CreateCTxPipeComm(char* name, void* msg);
+	TxPipeComm * CreateCTxPipeComm(char * name, void * msg);
 	void DestroyCTxPipeComm(TxPipeComm* ptr);
 
 	// TxPipeComm methods
-	void Send(TxPipeComm* ptr, void* msg);
-	void StartTx(TxPipeComm* ptr);
-	void StopTx(TxPipeComm* ptr);
-	void ResetTx(TxPipeComm* ptr);
-	int IsRunningTx(TxPipeComm* ptr);
+	void Send(TxPipeComm * ptr, void * msg);
+	void StartTx(TxPipeComm * ptr);
+	void StopTx(TxPipeComm * ptr);
+	void ResetTx(TxPipeComm * ptr);
+	int IsRunningTx(TxPipeComm * ptr);
 
 	//*********************************************************
 	// RxPipeComm
@@ -30,15 +30,15 @@ extern "C"
 	typedef struct RxPipeComm RxPipeComm;
 
 	// RxPipeComm constructor/destructor
-	RxPipeComm* CreateCRxPipeComm(char* name, void* msg);
-	void DestroyCRxPipeComm(RxPipeComm* ptr);
+	RxPipeComm * CreateCRxPipeComm(char * name, void * msg);
+	void DestroyCRxPipeComm(RxPipeComm * ptr);
 
 	// RxPipeComm methods
-	void* Get(RxPipeComm* ptr);
-	void StartRx(RxPipeComm* ptr);
-	void StopRx(RxPipeComm* ptr);
-	void ResetRx(RxPipeComm* ptr);
-	int IsRunningRx(RxPipeComm* ptr);
+	void * Get(RxPipeComm * ptr);
+	void StartRx(RxPipeComm * ptr);
+	void StopRx(RxPipeComm * ptr);
+	void ResetRx(RxPipeComm * ptr);
+	int IsRunningRx(RxPipeComm * ptr);
 
 #ifdef __cpusplus
 }
