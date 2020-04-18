6abfbbf2717454186fe0d642c102230b0d6355cd
aldhinn <aldjohn.spines@gmail.com>
2020-04-18 01:10:57 -0600

    Implemented StringMessage

--- __tests__/common/StringMessage/StringMessage.cpp
+++ __tests__/common/StringMessage/StringMessage.cpp
@@ -1,1 +1,11 @@
 #include "StringMessage.h"
+
+StringMessage::StringMessage(std::string value) {
+	this->value = value;
+}
+
+std::string StringMessage::ToString() {
+	return this->value;
+}
+
+void StringMessage::ToStruct(std::string str) {}
--- __tests__/common/StringMessage/StringMessage.h
+++ __tests__/common/StringMessage/StringMessage.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "PipeComm.h"
+
+class StringMessage : public IMessage {
+	std::string value;
+public:
+	StringMessage(std::string value);
+
+	std::string ToString();
+	void ToStruct(std::string str);
+};
