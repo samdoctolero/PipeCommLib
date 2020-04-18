dc202d00beb7bd0409d3b141d356b892c1edea6d
aldhinn <aldjohn.spines@gmail.com>
2020-04-18 01:59:05 -0600

    implemented RxApp

--- __tests__/test_apps/RxApp/RxApp.cpp
+++ __tests__/test_apps/RxApp/RxApp.cpp
@@ -1,5 +1,25 @@
+#include "PipeComm.h"
+#include "StringMessage.h"
 
+// The argument would be the message expected
 int main(int argc, char** argv)
 {
+	if (argc != 1) {
+		// Invalid number of args
+		return -1;
+	}
+
+	RxPipeComm* RxAppPipe =
+		new RxPipeComm("RxAppPipe", new StringMessage());
+	RxAppPipe->Start();
+
+	Sleep(2000);
+	IMessage* msg = RxAppPipe->Get();
+
+	// Return non-zero if msg doesn't match the argument
+	if (msg->ToString() != argv[0]) {
+		return -1;
+	}
+
 	return 0;
 }
