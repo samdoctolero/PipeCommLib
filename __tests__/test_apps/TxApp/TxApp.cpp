e08156a41efa8f252810d31cb0d1c40901e4ad51
aldhinn <aldjohn.spines@gmail.com>
2020-04-18 02:08:09 -0600

    TxApp implemented

--- __tests__/test_apps/TxApp/TxApp.cpp
+++ __tests__/test_apps/TxApp/TxApp.cpp
@@ -1,5 +1,24 @@
+#include "PipeComm.h"
+#include "StringMessage.h"
 
+// The argument would be the
+// string message to be sent
 int main(int argc, char** argv)
 {
+	if (argc != 1) {
+		// Invalid number of args
+		return -1;
+	}
+
+	TxPipeComm* TxAppPipe =
+		new TxPipeComm("RxAppPipe", new StringMessage());
+	TxAppPipe->Start();
+
+	Sleep(1000);
+	// Send the message specified in argv
+	TxAppPipe->Send(new StringMessage(argv[0]));
+
+	Sleep(3000);
+
 	return 0;
 }
